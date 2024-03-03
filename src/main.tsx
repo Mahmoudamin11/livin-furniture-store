0e9f1755c3cd89ca40076939d5199419a819bb48
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-03-03 09:51:37 +0200

    adding predeploy commands

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {  RouterProvider, createBrowserRouter} from 'react-router-dom';
-import AllProducts from './pages/AllProducts.tsx';
-import InCart from './pages/InCart.tsx';
-import Product from './components/Product.tsx';
-import Home from './pages/Home.tsx';
+import {  HashRouter} from 'react-router-dom';
+// import AllProducts from './pages/AllProducts.tsx';
+// import InCart from './pages/InCart.tsx';
+// import Product from './components/Product.tsx';
+// import Home from './pages/Home.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/livin-furniture-store/",
-    element: <App />,
-    children: [
-      {
-        path: "/livin-furniture-store/",
-        element: <Home />,
-      },
-      {
-        path: "/livin-furniture-store/AllProducts",
-        element: <AllProducts />,
-      },
-      {
-        path: "/livin-furniture-store/InCart",
-        element: <InCart />,
-      },
-      {
-        path: "/livin-furniture-store/Product",
-        element: <Product />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/livin-furniture-store/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/livin-furniture-store/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/livin-furniture-store/AllProducts",
+//         element: <AllProducts />,
+//       },
+//       {
+//         path: "/livin-furniture-store/InCart",
+//         element: <InCart />,
+//       },
+//       {
+//         path: "/livin-furniture-store/Product",
+//         element: <Product />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
